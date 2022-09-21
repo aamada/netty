b5b1be37384e2aa2248cdb1a9eff3a33b33f38a2
aamada <aamada@163.com>
2022-09-22 01:41:28 +0800

    加上行号

--- common/src/main/java/io/netty/util/cjm/utils/ThreadUtils.java
+++ common/src/main/java/io/netty/util/cjm/utils/ThreadUtils.java
@@ -3,10 +3,11 @@ package io.netty.util.cjm.utils;
 public class ThreadUtils {
     public static String getThreadInfo() {
         Thread thread = Thread.currentThread();
-        long id = thread.getId();
         String name = thread.getName();
-//        ThreadGroup threadGroup = thread.getThreadGroup();
-//        String name1 = threadGroup.getName();
-        return "id = " + id + ";name = " + name + ";======";
+        StackTraceElement[] stackTrace = thread.getStackTrace();
+        StackTraceElement stackTraceElement = stackTrace[4];
+        return "thread name = " + name + ";location=" +
+                stackTraceElement.getClassName() + "#" + stackTraceElement.getMethodName() +
+                ";lineNumber=" + stackTraceElement.getLineNumber();
     }
 }
